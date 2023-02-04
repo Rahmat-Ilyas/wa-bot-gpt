46775a82917d85a3873edbff456ff6aa20d5c337
Rahmat Ilyas <rahmat.ilyas142@gmail.com>
2023-02-04 10:30:39 +0800

    Revert "res"
    This reverts commit 4a1139bfa416d32b5b5c30600e0ad77ce8e5538f.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const { API_KEY_AI } = require('./config');
 
 const configuration = new Configuration({
-	apiKey: API_KEY_AI,
+	apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
